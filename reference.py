36dabbba5144f885f43d424d1381477480e64aab
Zach Josephson <zjosephson@gmail.com>
2012-03-06 21:17:19 -0800

    finished data structures chapter

---
+++ reference.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Filename: reference.py
+
+print('Simple Assignment')
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+mylist = shoplist
+
+del shoplist[0]
+print('shoplist is', shoplist)
+print('mylist is', mylist)
+
+print('Copy by making a full slice')
+mylist = shoplist[:]
+del shoplist[0]
+
+print('shoplist is', shoplist)
+print('mylist is', mylist)
---
+++ str_methods.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#Filename: str_methods.py
+
+name = 'Swaroop'
+
+if name.startswith('Swa'):
+    print('Yes, the string starts with "Swa"')
+
+if 'a' in name:
+    print('Yes, it contains the string "a"')
+
+if not name.find('hello') == -1:
+    print('Yes, it contains the string "war"')
+
+delimiter = '_*_'
+mylist = ['Brazil', 'Russia', 'India', 'China']
+print(delimiter.join(mylist))
